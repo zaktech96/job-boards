405998b34e955ae893e9aca497791cdd8fc81816
zaktech96 <zakariyesahid96@gmail.com>
2024-01-17 21:14:09 +0000

    text enlarged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
             <UserButton afterSignOutUrl="/"/>
       <Nav />
-      <h1 className="text-5xl font-bold mb-4">Job Board</h1>
+      <h1 className="text-7xl font-bold mb-4">Job Board</h1>
       <p className="text-lg">
         Welcome to our Job Board! Find exciting opportunities and take the next
         step in your career Journey.
