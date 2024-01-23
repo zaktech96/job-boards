3f70824481b68a4d1b3fa528cd8249056968435c
zaktech96 <zakariyesahid96@gmail.com>
2024-01-23 19:17:11 +0000

    file change up

--- src/app/Section/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Section() {
-    return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24">
-         <h1 className="text-5xl font-bold mb-4">Location section </h1>
-    </main>
-  );
-}
