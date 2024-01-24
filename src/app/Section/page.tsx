6e9d21a5d97e83a1b1a71c592f74ad296c4a078a
zaktech96 <zakariyesahid96@gmail.com>
2024-01-24 19:32:18 +0000

    dashboard implementation

--- src/app/Section/page.tsx
+++ src/app/Section/page.tsx
@@ -4,7 +4,7 @@ export default function Section() {
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
         <h1 className="text-5xl font-bold mb-4">Location section </h1>
         
-             <Link href="dashboard"> 
+             <Link href="/Dashboard"> 
         <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Go to Input Page
         </button>
