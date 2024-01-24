1a8d6384204da1596806d25e981f8dc42fec20d0
zaktech96 <zakariyesahid96@gmail.com>
2024-01-24 20:37:42 +0000

    dashboard implementation

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Dashboard } from "@/components/dashboard";
 export default function dashboard() {
     return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
-        <h1 className="text-5xl font-bold mb-4">Dashboard </h1>
+        <h1 className="text-5xl font-bold mb-4"> Dashboard-page </h1>
         
           
   <Dashboard/>
