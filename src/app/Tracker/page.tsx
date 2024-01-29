bb12b5c9a6fcb9f53a846011a71109da2ae6de49
zaktech96 <zakariyesahid96@gmail.com>
2024-01-29 23:43:47 +0000

    job-tracker implementation

---
+++ src/app/Tracker/page.tsx
@@ -0,0 +1,11 @@
+import { Tracker } from "@/components/tracker";
+export default function tracker() {
+    return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-24">
+        <h1 className="text-5xl font-bold mb-4"> Dashboard-page </h1>
+        
+          
+  <Tracker/>
+    </main>
+  );
+}
