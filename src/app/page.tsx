4667c1ccb90786ed85a3aad1d430587afe2a655e
zaktech96 <zakariyesahid96@gmail.com>
2024-01-30 23:33:46 +0000

    clerk-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
 
 
-  
+
       <Nav />
-        <UserButton afterSignOutUrl="/"/>
+          <UserButton afterSignOutUrl="/"/>
       <h1 className="text-5xl font-bold mb-4">Job Board</h1>
       <p className="text-lg">
         Welcome to our Job Board! Find exciting opportunities and take the next
